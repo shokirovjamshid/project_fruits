589c30969a2ea5f9254aaa742b4a6054c36a853d
shokirovjamshid <shokirovjamshid555@gmail.com>
2023-09-07 09:36:18 +0500

    This function returns the index of the cheapest fruit

--- get_cheapest_fruit_id.py
+++ get_cheapest_fruit_id.py
@@ -1,3 +1,4 @@
+from csv import reader
 def get_cheapest_fruit_id(data: str) -> int:
     """
     This function returns the index of the cheapest fruit
@@ -7,5 +8,16 @@ def get_cheapest_fruit_id(data: str) -> int:
     returns:
         int: id of the cheapest fruit
     """
-    # your code here
-    pass
+    f = open(data, mode='r+')
+    data_reader = list(reader(f))
+    price = []
+    for i in data_reader[1:]:
+        price.append(float(i[1]))
+    price_min = min(price)
+    index_min = price.index(price_min)+1
+    return index_min
+print(get_cheapest_fruit_id('fruits.csv'))
+    
+        
+        
+            
