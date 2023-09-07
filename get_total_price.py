e88edb49dfcf45e636199d3e02db6a0c78e9243f
shokirovjamshid <shokirovjamshid555@gmail.com>
2023-09-07 17:37:11 +0500

    This function returns the total price of the fruits

--- get_total_price.py
+++ get_total_price.py
@@ -10,9 +10,9 @@ def get_total_price(data:str)->float:
     """
     f = open(data, mode='r+')
     data_reader = list(reader(f))
-    price = []
+    price = 0
     for i in data_reader[1:]:
-        price.append(float(i[1]))
+        price+=float(i[1])
     return price
 
     
